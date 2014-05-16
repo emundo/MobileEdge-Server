b0e7560fc0a3fb93b5205d2aedc8854cff2d02ac
Raphael Arias <arias@in.tum.de>
2014-05-16 17:07:32 +0200

    added https support

--- node/main.js
+++ node/main.js
@@ -1,6 +1,12 @@
-var http = require("http");
+var https = require("https");
+var fs = require('fs');
 
-http.createServer(function(request, response) {
+var options = {
+  key: fs.readFileSync('server.key'),
+  cert: fs.readFileSync('server.crt'),
+  ca: [fs.readFileSync('cacert.pem')]
+};
+https.createServer(options, function(request, response) {
   response.writeHead(200, {"Content-Type": "text/plain"});
   response.write("Hello World");
   response.end();
