4ad654a6b20a935bf9876e673b3e06f38ef40faf
Raphael Arias <arias@in.tum.de>
2014-05-23 16:52:25 +0200

    added token generation and some handy functions in util.js lalal
    Change-Id: Idecb7ba7ed70691df73948f4e10daf0303929419

---
+++ node/libs/token.js
@@ -0,0 +1,50 @@
+/**
+ * @file Implements token-related operations like token creation.
+ * @author Raphael Arias <raphael.arias@e-mundo.de>
+ */
+var nacl_factory = require("js-nacl");
+var nacl = nacl_factory.instantiate();
+var util = require("./util.js");
+console.log(nacl.to_hex(nacl.random_bytes(16)));
+
+cv = nacl.encode_utf8
+toHex = nacl.to_hex
+
+key = cv("d2cf52201f5fe179a9174b3925351159")
+
+/**
+ * Creates an ID-Token from data and calls a given callback function
+ * to hand this token back.
+ * Beware: The argument data is not checked, nor is it filled with appropriate
+ * data, such as a Nonce/Timestamp. This should be taken care of from the
+ * calling function, yet to define.
+ * @param data - data to be included into token
+ * @param callback - callback function to be called when token was generated.
+ */
+function create_id(data, callback) {
+    token = {
+        'info' : data,
+        'mac' : toHex(hmac(key, data))
+    }; 
+    return token; //TODO: change this to callback(token);
+}
+
+/**
+ * Compute an HMAC with a given key and data (message).
+ * The inner and outer padding (ipad, opad) are based on the 
+ * RFC2104 definition of HMAC (https://tools.ietf.org/html/rfc2104) 
+ * @param {Uint8Array} key - the secret key used for the HMAC
+ * @param {string} data - the data to be embedded in the HMAC
+ */
+function hmac(key, data) {
+    opad = new Uint8Array(64); 
+    for (var i = 0; i < opad.length; i++)
+        opad[i] = 0x5c;
+    ipad = new Uint8Array(64); 
+    for (var i = 0; i < ipad.length; i++)
+        ipad[i] = 0x5c;
+
+    return nacl.crypto_hash_sha256(cv(util.xor(key,opad) + nacl.crypto_hash_sha256(cv(toHex(util.xor(key, ipad)) + data))));
+}
+
+exports.create_id = create_id;
--- node/main.js
+++ node/main.js
@@ -1,5 +1,7 @@
 var https = require('https'),
     fs = require('fs');
+var token = require("./libs/token.js");
+var util = require("./libs/util.js");
 var sslOptions = {
   key: fs.readFileSync('./ssl/server.key'),
   cert: fs.readFileSync('./ssl/server.crt'),
@@ -12,5 +14,7 @@ var secureServer = https.createServer(sslOptions,function(request, response) {
   response.write("Hello World");
   response.end();
 }).listen('8888', function(){
-  console.log("Secure Express server listening on port 8888");
+  console.log("Secure server listening on port 8888");
+  console.log(token.create_id("hi there!!! ", null));
+  console.log(util.xor("AAAA", "ZZZZ"));
 });
