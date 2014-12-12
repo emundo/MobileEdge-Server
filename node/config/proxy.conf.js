613a8efb3fbc960f4c998affab55ed9ef4501aa0
Raphael Arias <raphael.arias@e-mundo.de>
2014-12-12 10:40:12 +0100

    nicer style in proxy config

--- node/config/proxy.conf.js
+++ node/config/proxy.conf.js
@@ -1,7 +1,17 @@
 var config = {
-    host : '127.0.0.1', // which IP/hostname should the MobileEdge server forward the requests to?
-    port : 6969, // on which port is the backend server listening (TCP)
-    terminationSeq : new Buffer('\r\n\r\n') // which sequence terminates a response from the backend?
+    /* 
+     * which IP/hostname should the MobileEdge
+     * server forward the requests to?
+     */
+    host : '127.0.0.1',
+    /* 
+     * on which port is the backend server listening (TCP) 
+     */
+    port : 6969, 
+    /*
+     * which sequence terminates a response from the backend?
+     */
+    terminationSeq : new Buffer('\r\n\r\n') 
 };
 
 exports.proxyConfiguration = config; 
