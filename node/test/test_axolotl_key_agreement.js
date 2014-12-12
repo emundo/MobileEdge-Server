bc94e34a851b85791006f28549b0ccccd21ff692
Raphael Arias <raphael.arias@e-mundo.de>
2014-12-12 12:43:07 +0100

    license headers added

--- node/test/test_axolotl_key_agreement.js
+++ node/test/test_axolotl_key_agreement.js
@@ -1,12 +1,20 @@
 /*
+ * This file is part of MobileEdge-Server, the server-side component
+ * of the MobileEdge framework.
  * Copyright (c) 2014 eMundo GmbH
- * All Rights Reserved.
- *
- * This software is the confidential and proprietary information of
- * eMundo GmbH. ("Confidential Information"). You
- * shall not disclose such Confidential Information and shall use it
- * only in accordance with the terms of the license agreement you
- * entered into with eMundo GmbH.
+
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  *
  * Created by Raphael Arias on 2014-06-02.
  */
